fcd03d6fbaab4556c96d4298d84edfce7cc71c5d
Edward Sutton <edward.sutton@subsite.com>
2016-08-05 14:35:29 -0500

    Example templates apps running

--- example/index.ios.js
+++ example/index.ios.js
@@ -3,51 +3,11 @@
  * https://github.com/facebook/react-native
  * @flow
  */
+ import { AppRegistry } from 'react-native';
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+ // @todo remove when RN upstream is fixed
+//console.ignoredYellowBox = ['Warning: Failed propType: SceneView'];
 
-class BluetoothIOExample extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to BluetoothIOExample!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+ import BluetoothIOExample from './BluetoothIOExample';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('BluetoothIOExample', () => BluetoothIOExample);
+ AppRegistry.registerComponent('BluetoothIOExample', () => BluetoothIOExample);
